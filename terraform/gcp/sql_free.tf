936115349f436b6fbd7d209492acaac9cc5b4d69
nsoma-stackod <nsoma@stackoverdrive.io>
2026-09-22 16:27:05 -0400

    feat: complete raw kubernetes templates, monitoring, and database configurations

--- terraform/gcp/sql_free.tf
+++ terraform/gcp/sql_free.tf
@@ -3,7 +3,7 @@ resource "google_sql_database_instance" "free_instance" {
   database_version = "POSTGRES_15"
   region           = "us-central1"
   settings {
-    tier              = "db-f1-micro" # Lowest credit consumption tier
+    tier              = "db-f1-micro" # Shared-core micro free sandbox
     activation_policy = "ALWAYS"
     availability_type = "ZONAL"
   }
